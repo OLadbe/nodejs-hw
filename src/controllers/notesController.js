9efe7f82ae3ef5650869d0410ba80e61f9651aaf
Taras <taras.zhovanyk@icloud.com>
2026-09-10 23:44:33 +0200

    Correction 5 question

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -4,12 +4,12 @@ import createHttpError from 'http-errors';
 
 export const getAllNotes = async (req, res) => {
   const notes = await Note.find();
-  res.status(200).json( notes );
+  res.status(200).json(notes);
 };
 
 export const getNoteById = async (req, res) => {
   const { noteId } = req.params;
-  const note = Note.findById(noteId);
+  const note = await Note.findById(noteId);
   if (!note) {
     throw createHttpError(404, `Note with ID ${noteId} not found`);
   }
@@ -23,7 +23,7 @@ export const createNote = async (req, res) => {
 
 export const updateNote = async (req, res) => {
   const { noteId } = req.params;
-  const note = await Note.findOneAndUpdate(noteId, req.body, { returnDocument: 'after' });
+  const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, { returnDocument: 'after' });
   if (!note) {
     throw createHttpError(404, `Note not found`);
   }
