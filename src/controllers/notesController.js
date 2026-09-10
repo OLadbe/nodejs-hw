6d627ceb409239cfe0d09a9359b72532a051b11c
Taras <taras.zhovanyk@icloud.com>
2026-09-10 23:53:47 +0200

    Add noteId to errors and returning json object

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -13,7 +13,7 @@ export const getNoteById = async (req, res) => {
   if (!note) {
     throw createHttpError(404, `Note with ID ${noteId} not found`);
   }
-  res.status(200).json({ "message": `Retrieved note with ID: ${noteId}` });
+  res.status(200).json(note);
  };
 
 export const createNote = async (req, res) => {
@@ -25,7 +25,7 @@ export const updateNote = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findOneAndUpdate({ _id: noteId }, req.body, { returnDocument: 'after' });
   if (!note) {
-    throw createHttpError(404, `Note not found`);
+    throw createHttpError(404, `Note ${noteId} not found`);
   }
   res.status(200).json(note);
 };
@@ -34,7 +34,7 @@ export const deleteNote = async (req, res) => {
   const { noteId } = req.params;
   const note = await Note.findOneAndDelete({ _id: noteId });
   if (!note) {
-    throw createHttpError(404, `Note not found`);
+    throw createHttpError(404, `Note ${noteId} not found`);
   }
   res.status(200).json(note);
 };
